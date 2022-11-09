f739e8975159c1a55a8eb68e7cdb840ec3636f3a
Sebastian Zapata Mardini <sebastianza@zillowgroup.com>
2022-11-09 17:08:37 -0500

    Lifting state up for adding expense filtering by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2020");
+
+  const onChangeFilterHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selectedYear={selectedYear}
+        onChangeFilter={onChangeFilterHandler}
+      />
       {props.items.map((expense) => (
         <ExpenseItem
           title={expense.title}
