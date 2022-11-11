6bd2b8f9a3f76c7ee8531bcefb8478f7e2df3ac8
Sebastian Zapata Mardini <sebastianza@zillowgroup.com>
2022-11-11 12:51:33 -0500

    Define support to dynamically add expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,19 +13,22 @@ const Expenses = (props) => {
   };
 
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selectedYear={selectedYear}
-        onChangeFilter={onChangeFilterHandler}
-      />
-      {props.items.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selectedYear={selectedYear}
+          onChangeFilter={onChangeFilterHandler}
         />
-      ))}
-    </Card>
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 };
 
